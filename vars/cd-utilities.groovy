b16a8a9e58afd27bdac59feac3a0f572f700367c
nugensol <venkat_komm@yahoo.com>
2021-08-16 14:50:32 -0400

    Update cd-utilities.groovy

--- vars/cd-utilities.groovy
+++ vars/cd-utilities.groovy
@@ -1,1 +1,12 @@
-
+/**
+* Maven Windows Unit testing
+*/
+def doUnitTestForMavenOnWindows(){
+  	bat "c:\\jenkins_home\\apps\\maven-3.0\\bin\\mvn test"
+}
+/**
+* Maven Linux Unit testing
+*/
+def doUnitTestForMavenOnLinux(){
+  	sh "/usr/jenkins_home/apps/maven-3.0/bin/mvn test"
+}
