1af6fc1570a9e87cb9b31748b113e14a58f9ce5d
nugensol <venkat_komm@yahoo.com>
2021-08-30 23:43:08 -0400

    Update lambda.groovy

--- common/scripts/aws-cli/lambda.groovy
+++ common/scripts/aws-cli/lambda.groovy
@@ -7,3 +7,26 @@ def create_lambda_function(){
     --role arn:aws:iam::123456789012:role/service-role/MyTestFunction-role-tges6bf4
   """
 }
+
+def update_lambda_function_env_variables(){
+  sh """
+    aws lambda update-function-configuration \
+      --function-name  my-function \
+      --memory-size 256
+    """
+}
+
+def update_lambda_function_code(){
+  sh """
+    aws lambda update-function-code \
+        --function-name  my-function \
+        --zip-file fileb://my-function.zip
+      """
+}
+
+def publish_version(){
+ sh """ 
+ aws lambda publish-version \
+    --function-name my-function
+    """
+}
