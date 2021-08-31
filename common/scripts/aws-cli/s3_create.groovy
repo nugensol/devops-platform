45523c2164bf19d7e510bc9bc1e75c51ed86f5a4
nugensol <venkat_komm@yahoo.com>
2021-08-30 22:42:53 -0400

    Update s3_create.groovy

--- common/scripts/aws-cli/s3_create.groovy
+++ common/scripts/aws-cli/s3_create.groovy
@@ -1,3 +1,9 @@
-aws s3 mb s3://linux-is-awesome --region eu-central-1
+void create_s3_bucket(){
+  sh "aws s3 mb s3://linux-is-awesome --region eu-central-1"
   
   sh "aws s3 website s3://linux-is-cool --index-document index.html --error-document error.html"
+}
+void copy_s3_bucket(){
+ sh "sh "aws s3 cp new.txt s3://linux-is-awesome"" 
+}
+
