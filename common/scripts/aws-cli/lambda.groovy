d5df4a2182a1fce0531133cebaf5ed21d907d24b
nugensol <venkat_komm@yahoo.com>
2021-08-30 23:35:17 -0400

    Update lambda.groovy

--- common/scripts/aws-cli/lambda.groovy
+++ common/scripts/aws-cli/lambda.groovy
@@ -1,1 +1,9 @@
-
+def create_lambda_function(){
+  sh """aws lambda create-function \
+    --function-name my-function \
+    --runtime nodejs10.x \
+    --zip-file fileb://my-function.zip \
+    --handler my-function.handler \
+    --role arn:aws:iam::123456789012:role/service-role/MyTestFunction-role-tges6bf4
+  """
+}
